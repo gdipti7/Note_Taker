43b8c8e21e0ae1602e000c6c009594a9d73bf1e6
gdipti7 <diptigiri443@gmail.com>
2026-06-29 11:19:24 +0545

    Add NoteGrid component

--- src/components/NoteGrid.jsx
+++ src/components/NoteGrid.jsx
@@ -1,10 +1,10 @@
 import NoteCard from "./NoteCard";
 
-export default function NoteGrid({ notes }) {
+export default function NoteGrid({ notes, onDelete }) {
   return (
     <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-5">
       {notes.map((note) => (
-        <NoteCard key={note.id} note={note} />
+        <NoteCard key={note.id} note={note} onDelete={onDelete} />
       ))}
     </div>
   );
