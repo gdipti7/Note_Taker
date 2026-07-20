d8c9f96de02ba5d0c19f37b3513ec1148ebfeb84
gdipti7 <diptigiri443@gmail.com>
2026-07-20 11:05:06 +0545

    Add noteApi.js for backend API integration

---
+++ src/api/noteApi.js
@@ -0,0 +1,28 @@
+import axios from 'axios'
+
+const API_URL = 'http://localhost:5000/api/notes'
+
+export const getAllNotes = async () => {
+  const response = await axios.get(API_URL)
+  return response.data
+}
+
+export const getNoteById = async (id) => {
+  const response = await axios.get(`${API_URL}/${id}`)
+  return response.data
+}
+
+export const createNote = async (noteData) => {
+  const response = await axios.post(API_URL, noteData)
+  return response.data
+}
+
+export const updateNote = async (id, noteData) => {
+  const response = await axios.put(`${API_URL}/${id}`, noteData)
+  return response.data
+}
+
+export const deleteNote = async (id) => {
+  const response = await axios.delete(`${API_URL}/${id}`)
+  return response.data
+}
