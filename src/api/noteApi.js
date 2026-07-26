d65889881cfb0aeb5e5147ededb4cc23ef7faf28
gdipti7 <diptigiri443@gmail.com>
2026-07-26 22:45:59 +0545

    Add auth token header to createNote, updateNote, and deleteNote requests

--- src/api/noteApi.js
+++ src/api/noteApi.js
@@ -2,6 +2,11 @@ import axios from 'axios'
 
 const API_URL = 'http://localhost:5000/api/notes'
 
+const getAuthHeader = () => {
+  const token = localStorage.getItem('token')
+  return { headers: { Authorization: `Bearer ${token}` } }
+}
+
 export const getAllNotes = async () => {
   const response = await axios.get(API_URL)
   return response.data
@@ -13,16 +18,16 @@ export const getNoteById = async (id) => {
 }
 
 export const createNote = async (noteData) => {
-  const response = await axios.post(API_URL, noteData)
+  const response = await axios.post(API_URL, noteData, getAuthHeader())
   return response.data
 }
 
 export const updateNote = async (id, noteData) => {
-  const response = await axios.put(`${API_URL}/${id}`, noteData)
+  const response = await axios.put(`${API_URL}/${id}`, noteData, getAuthHeader())
   return response.data
 }
 
 export const deleteNote = async (id) => {
-  const response = await axios.delete(`${API_URL}/${id}`)
+  const response = await axios.delete(`${API_URL}/${id}`, getAuthHeader())
   return response.data
 }
