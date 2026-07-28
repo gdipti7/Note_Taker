c1cc9831ad428e2b38ebe32245ed76e46c7d70f4
gdipti7 <diptigiri443@gmail.com>
2026-07-28 11:53:04 +0545

    Use deployed backend URL for auth requests

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,13 +1,14 @@
 import axios from 'axios'
 
-const API_URL = '/auth'
+
+const AUTH_URL = 'https://note-taker-backend-59x2.onrender.com/auth'
 
 export const registerUser = async (formData) => {
-  const response = await axios.post(`${API_URL}/register`, formData, { withCredentials: true })
+  const response = await axios.post(`${AUTH_URL}/register`, formData, { withCredentials: true })
   return response.data
 }
 
 export const loginUser = async (formData) => {
-  const response = await axios.post(`${API_URL}/login`, formData, { withCredentials: true })
+  const response = await axios.post(`${AUTH_URL}/login`, formData, { withCredentials: true })
   return response.data
 }
